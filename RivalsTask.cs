f8c65d2689705c49659e2dad8a22368f3be09956
Pahuka <J-a-c-k@yandex.ru>
2021-05-11 08:39:41 +0500

    Завершил реализацию метода AssignOwners. Задание сдано.

--- RivalsTask.cs
+++ RivalsTask.cs
@@ -1,61 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 
 namespace Rivals
 {
-	public class RivalsTask
-	{
-		public static IEnumerable<OwnedLocation> AssignOwners(Map map)
-		{
-            var visitPoint = new HashSet<Point>();
-            var queue = new Queue<Point>();
-            var result = new Dictionary<Point, OwnedLocation>();
+    public class RivalsTask
+    {
+        public static IEnumerable<OwnedLocation> AssignOwners(Map map)
+        {
+            var queue = new Queue<Tuple<Point, OwnedLocation>>();
+            var visitPoints = new HashSet<Point>();
 
             for (int i = 0; i < map.Players.Length; i++)
             {
-                result.Add(map.Players[i], new OwnedLocation(i, map.Players[i], 0));
-                queue.Enqueue(map.Players[i]);
+                queue.Enqueue(Tuple.Create(map.Players[i], new OwnedLocation(i, map.Players[i], 0)));
+                visitPoints.Add(map.Players[i]);
             }
-
-            visitPoint.Add(map.Players.First());
 
             while (queue.Count != 0)
             {
-                var point = queue.Dequeue();
+                var owner = queue.Dequeue();
 
-                if (map.Maze[point.X, point.Y] != MapCell.Empty) continue;
-
-                PointGeneration(visitPoint, map, queue, result, point);
-            }
-
-            foreach (var item in result)
-            {
-                yield return item.Value;
-            }
-            yield break;
-        }
-
-        public static void PointGeneration(HashSet<Point> visitPoint, Map map,
-            Queue<Point> queue, Dictionary<Point, OwnedLocation> result, Point point)
-        {
-            for (var dy = -1; dy <= 1; dy++)
-                for (var dx = -1; dx <= 1; dx++)
-                    if (dx != 0 && dy != 0) continue;
-                    else
-                    {
-                        var tPoint = new Point { X = point.X + dx, Y = point.Y + dy };
-                        if (!visitPoint.Contains(tPoint) & map.InBounds(tPoint) & !queue.Contains(tPoint))
+                for (var dy = -1; dy <= 1; dy++)
+                    for (var dx = -1; dx <= 1; dx++)
+                        if (dx != 0 && dy != 0) continue;
+                        else
                         {
-                            if (map.Maze[tPoint.X, tPoint.Y] != MapCell.Empty) continue;
-                            queue.Enqueue(tPoint);
-                            visitPoint.Add(tPoint);
-                            var owned = result.ContainsKey(tPoint) ? 
-                                result[tPoint] :
-                                new OwnedLocation(result[point].Owner, tPoint, result[point].Distance + 1);
-                            result[tPoint] = owned;
+                            var tPoint = new Point { X = owner.Item1.X + dx, Y = owner.Item1.Y + dy };
+                            if (map.InBounds(tPoint) & !visitPoints.Contains(tPoint))
+                            {
+                                if (map.Maze[tPoint.X, tPoint.Y] != MapCell.Empty) continue;
+                                queue.Enqueue(Tuple.Create(tPoint,
+                                    new OwnedLocation(owner.Item2.Owner, tPoint, owner.Item2.Distance + 1)));
+                                visitPoints.Add(tPoint);
+                            }
                         }
-                    }
+                yield return owner.Item2;
+            }
         }
     }
 }
